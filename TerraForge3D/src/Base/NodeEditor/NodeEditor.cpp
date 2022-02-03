ad8574c4074d5f2403012fba970de8471765c824
ExcitedElf <95520623+ExcitedElf@users.noreply.github.com>
2022-02-03 12:48:42 +0530

    Update NodeEditor.cpp

--- TerraForge3D/src/Base/NodeEditor/NodeEditor.cpp
+++ TerraForge3D/src/Base/NodeEditor/NodeEditor.cpp
@@ -453,7 +453,7 @@ void NodeEditor::AddNode(NodeEditorNode* node)
         pins[it->_id.Get()] = it;
     }
     node->Setup();
-    if (nodes.size() > 2)
+    if (nodes.size() > 2 && nodes.find(lastNodeId.Get()) != nodes.end())
     {
         NodeEditorNode* lastNode = nodes[lastNodeId.Get()];
         node->nodePosition = lastNode->nodePosition;
