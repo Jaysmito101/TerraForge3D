c6825939f6ac42d50a3e2eebcf0b19542f19c566
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2022-03-08 15:41:19 +0530

    Fix

--- scripts/DeployDocs.bat
+++ scripts/DeployDocs.bat
@@ -6,4 +6,16 @@ vendor\\doxygen\\doxygen
 
 git clone --single-branch --branch gh-pages https://github.com/Jaysmito101/TerraForge3D ./ghpages
 
-(robocopy Docs\\html\\ ghpages\\documentation\\ /s /e /MT /IS /IT /IM)  ^& exit 0
+cd ..
+
+mkdir TEMP 
+
+(robocopy TerraForge3D\\Docs\\html\\ TEMP\\ /s /e /MT /IS /IT /IM)  ^& exit 0
+
+cd TerraForge3D
+
+git checkout gh-pages
+
+cd ..
+
+(robocopy TEMP\\ TerraForge3D\\documentation\\ /s /e /MT /IS /IT /IM)  ^& exit 0
