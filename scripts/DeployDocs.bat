6fa84f7aaa1da244827f9f9030d30848afa542fc
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2022-03-08 15:18:19 +0530

    Fixed aa bug

--- scripts/DeployDocs.bat
+++ scripts/DeployDocs.bat
@@ -6,6 +6,8 @@ vendor\\doxygen\\doxygen
 
 git clone --single-branch --branch gh-pages https://github.com/Jaysmito101/TerraForge3D ./ghpages
 
-mkdir ghpages\\documentation > nul
+del /f /s /q ghpages\\documentation 1>nul
+
+rmdir /s /q ghpages\\documentation
 
 xcopy Docs\\html ghpages\\documentation\\ /E /T /H /C /R /Q /Y
