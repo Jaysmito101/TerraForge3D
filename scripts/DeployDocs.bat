e12715bc59b368b683cf0a63544a1cc7c9717bec
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2022-03-08 14:35:54 +0530

    Again Bug Fix

--- scripts/DeployDocs.bat
+++ scripts/DeployDocs.bat
@@ -6,4 +6,4 @@ vendor\\doxygen\\doxygen
 
 git clone --single-branch --branch gh-pages https://github.com/Jaysmito101/TerraForge3D ./ghpages
 
-xcopy Docs\\html\\ ghpages\\documentation\\  /E /H /C /R /Q /Y
+xcopy Docs\\html ghpages\\documentation\\ /E /H /C /R /Q /Y
