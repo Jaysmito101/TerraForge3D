f607389fc9f04305753f94413fcb005447d908b5
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2022-03-23 19:22:24 +0530

    Added Advanced PBR Renderer

--- Binaries/Data/shader_nodes/extras.glsl
+++ Binaries/Data/shader_nodes/extras.glsl
@@ -58,3 +58,40 @@ float voronoise( in vec2 p, float u, float v )
 	
     return a.x/a.y;
 }
+
+// From : https://github.com/Nadrin/PBR/blob/master/data/shaders/glsl/pbr_fs.glsl#L148
+
+// Constant normal incidence Fresnel factor for all dielectrics.
+const vec3 Fdielectric = vec3(0.04);
+const float Epsilon = 0.00001;
+
+// GGX/Towbridge-Reitz normal distribution function.
+// Uses Disney's reparametrization of alpha = roughness^2.
+float ndfGGX(float cosLh, float roughness)
+{
+	float alpha   = roughness * roughness;
+	float alphaSq = alpha * alpha;
+
+	float denom = (cosLh * cosLh) * (alphaSq - 1.0) + 1.0;
+	return alphaSq / (3.141f * denom * denom);
+}
+
+// Single term for separable Schlick-GGX below.
+float gaSchlickG1(float cosTheta, float k)
+{
+	return cosTheta / (cosTheta * (1.0 - k) + k);
+}
+
+// Schlick-GGX approximation of geometric attenuation function using Smith's method.
+float gaSchlickGGX(float cosLi, float cosLo, float roughness)
+{
+	float r = roughness + 1.0;
+	float k = (r * r) / 8.0; // Epic suggests using this roughness remapping for analytic lights.
+	return gaSchlickG1(cosLi, k) * gaSchlickG1(cosLo, k);
+}
+
+// Shlick's approximation of the Fresnel factor.
+vec3 fresnelSchlick(vec3 F0, float cosTheta)
+{
+	return F0 + (vec3(1.0) - F0) * pow(1.0 - cosTheta, 5.0);
+}
