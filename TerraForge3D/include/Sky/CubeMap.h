b0fdd3d508de064a768b8a3a5720c6a428f05c86
Animesh Kumar Singh <tronuprisinglearn@gmail.com>
2023-02-09 17:00:04 +0530

    Added comments in CubeMap.h

--- TerraForge3D/include/Sky/CubeMap.h
+++ TerraForge3D/include/Sky/CubeMap.h
@@ -11,14 +11,23 @@ class Model;
 class CubeMapManager
 {
 public:
+	//! CubeMapManager Constructor
+	/*!
+		For Internal Use Only
+	*/
 	CubeMapManager(ApplicationState *appState);
+	
+	//! CubeMapManager Destructor
+	/*!
+		For Internal Use Only
+	*/
 	~CubeMapManager();
 
 	void RenderSkybox(glm::mat4 view, glm::mat4 proj, bool useBox, bool useProcedural, float cirrus, float cumulus, float time, float *fsun, float upF);
 
 	TextureCubemap *GetSkyboxCubemapTexture();
 public:
-	ApplicationState *appState = nullptr;
+	ApplicationState *appState = nullptr; /*!< The ApplicationState allows you to control anything in TerraForge3D */
 	Shader *skyboxShader = nullptr;
 	Shader *skyproShader = nullptr;
 	TextureCubemap *cubemap = nullptr;
