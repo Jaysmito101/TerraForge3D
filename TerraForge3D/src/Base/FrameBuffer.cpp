b9988ac10f6188c939d749ea3d49d5b167779180
jaysmito <jaysmito101@gmail.com>
2023-03-21 12:28:13 +0530

    Fixed Workflow Issue

--- TerraForge3D/src/Base/FrameBuffer.cpp
+++ TerraForge3D/src/Base/FrameBuffer.cpp
@@ -10,7 +10,7 @@ FrameBuffer::FrameBuffer(int w, int h)
 	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
 	glGenTextures(1, &colorTexture);
 	glBindTexture(GL_TEXTURE_2D, colorTexture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorTexture, 0);
