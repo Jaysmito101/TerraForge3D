07da76495c54e779d63950d0a3acb632e6363629
Jaysmito101 <jaysmito101@gmail.com>
2026-08-04 04:55:14 +0530

    feat: add mask_copy shader for texture processing in terrain generation

---
+++ Binaries/Data/shaders/generation/utils/mask_copy.glsl
@@ -0,0 +1,18 @@
+#version 430 core
+
+layout(local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
+
+layout(binding = 0) uniform sampler2D u_SourceMask;
+layout(binding = 1, r16) uniform image2D u_DestinationMask;
+
+uniform int u_Resolution;
+
+void main()
+{
+	ivec2 coordinate = ivec2(gl_GlobalInvocationID.xy);
+	if (coordinate.x >= u_Resolution || coordinate.y >= u_Resolution) return;
+
+	vec2 uv = (vec2(coordinate) + vec2(0.5)) / float(u_Resolution);
+	float value = texture(u_SourceMask, uv).r;
+	imageStore(u_DestinationMask, coordinate, vec4(value, 0.0, 0.0, 1.0));
+}
