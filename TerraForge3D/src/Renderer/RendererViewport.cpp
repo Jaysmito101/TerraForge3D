b30fb1c4618a12138bfcc9cccaedec60f0b6ff88
Jaysmito101 <jaysmito101@gmail.com>
2026-08-05 03:10:32 +0530

    feat: improve RendererViewport::ResizeTo by adding checks for zero dimensions and redundant resizing

--- TerraForge3D/src/Renderer/RendererViewport.cpp
+++ TerraForge3D/src/Renderer/RendererViewport.cpp
@@ -12,6 +12,10 @@ RendererViewport::~RendererViewport()
 
 void RendererViewport::ResizeTo(uint32_t width, uint32_t height)
 {
+	if (width == 0 || height == 0) return;
+	if (m_FrameBuffer && m_Width == static_cast<int32_t>(width) && m_Height == static_cast<int32_t>(height)) return;
+
 	m_Width = width; m_Height = height;
+	m_AspectRatio = static_cast<float>(width) / static_cast<float>(height);
 	m_FrameBuffer = std::make_shared<FrameBuffer>(width, height);
 }
