f65af434f62f011d5e29ccba1334d8d369160ce4
Jaysmito101 <jaysmito101@gmail.com>
2026-08-07 00:49:27 +0530

    feat: refactor SeaRenderer to utilize CustomInspector for state management and remove Settings struct

--- TerraForge3D/include/Renderer/SeaRenderer.h
+++ TerraForge3D/include/Renderer/SeaRenderer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Exporters/Serializer.h"
+#include "Misc/CustomInspector.h"
 #include "Renderer/RendererBase.h"
 
 #include <cstdint>
@@ -10,59 +12,43 @@ namespace tf3d::renderer
     class SeaRenderer : public RendererBase
     {
     public:
-        struct Settings {
-            bool enabled               = true;
-            float seaLevel             = 0.0f;
-            float waveAmplitude        = 0.022f;
-            float waveLength           = 0.18f;
-            float waveSpeed            = 0.16f;
-            float waveChoppiness       = 0.36f;
-            float shoreWidth           = 0.055f;
-            float shoreSoftness        = 0.35f;
-            float deepDepth            = 0.48f;
-            float normalStrength       = 0.20f;
-            float normalScale          = 4.2f;
-            float refractionStrength   = 0.006f;
-            float reflectionStrength   = 0.52f;
-            float opacity              = 0.96f;
-            float foamStrength         = 0.36f;
-            float nearshoreFoamWidth   = 0.18f;
-            float offshoreFoamStrength = 0.08f;
-            float foamScale            = 5.5f;
-            float foamSpeed            = 0.24f;
-            glm::vec3 shallowColor     = glm::vec3(0.012f, 0.19f, 0.23f);
-            glm::vec3 deepColor        = glm::vec3(0.002f, 0.018f, 0.055f);
-            glm::vec3 foamColor        = glm::vec3(0.56f, 0.76f, 0.76f);
-        };
-
         explicit SeaRenderer(ApplicationState *appState);
         ~SeaRenderer() override;
 
         void Render(RendererViewport *viewport) override;
         void ShowSettings() override;
 
+        inline exporters::SerializerNode Save() const
+        {
+            return m_Inspector.SaveState();
+        }
+
+        inline bool Load(exporters::SerializerNode data)
+        {
+            return m_Inspector.LoadState(data);
+        }
+
+        inline nlohmann::json GetSettingsSchema() const
+        {
+            return m_Inspector.BuildSchema();
+        }
+
         inline bool IsEnabled() const
         {
-            return m_Settings.enabled;
-        }
-        inline Settings &GetSettings()
-        {
-            return m_Settings;
-        }
-        inline const Settings &GetSettings() const
-        {
-            return m_Settings;
+            return m_Inspector.Get<bool>("Enabled");
         }
 
     protected:
         void ReloadShaders() override;
 
     private:
+        void BuildInspector();
+
         void BindUniforms(RendererViewport *viewport, float terrainWorldSize,
                           float terrainHeightOffset, float seaWorldHeight,
                           const glm::vec2 &surfaceMinimumXZ, const glm::vec2 &surfaceWorldSize);
 
-        Settings m_Settings;
+        CustomInspector m_Inspector;
         uint32_t m_Vao      = 0;
         float m_ElapsedTime = 0.0f;
     };
