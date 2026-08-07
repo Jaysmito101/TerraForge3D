0dcdd0ccc4e3adc330f54943e06e0e215489d397
Jaysmito101 <jaysmito101@gmail.com>
2026-08-07 19:51:11 +0530

    feat: remove ShaderUtils and integrate shader compilation directly in Shader.cpp

--- TerraForge3D/src/Base/Shader.cpp
+++ TerraForge3D/src/Base/Shader.cpp
@@ -1,23 +1,68 @@
 #include <Base.h>
+#include <Base/Logging/Logger.h>
 #include <Shader.h>
-#include <ShaderUtils.h>
 #include <glm/gtc/type_ptr.hpp>
 
 namespace tf3d::base
 {
+
+    namespace
+    {
+        GLuint CompileShader(const std::string &shaderSrc, GLenum shaderType, const std::string &name)
+        {
+            GLuint shader        = glCreateShader(shaderType);
+            const GLchar *source = shaderSrc.c_str();
+            glShaderSource(shader, 1, &source, 0);
+            glCompileShader(shader);
+            GLint isCompiled = 0;
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
+
+            if (isCompiled == GL_FALSE) {
+                GLint maxLength = 0;
+                glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+                char *errorLog = (char *)malloc(maxLength);
+                memset(errorLog, 0, maxLength);
+                glGetShaderInfoLog(shader, maxLength, &maxLength, errorLog);
+                TF3D_LOG_ERROR("Failed to compile {} shader", name);
+                TF3D_LOG_DEBUG("Shader source:\n{}", shaderSrc);
+                TF3D_LOG_ERROR("Shader compiler output: {}", errorLog);
+                glDeleteShader(shader);
+                free(errorLog);
+                return 0;
+            }
+
+            return shader;
+        }
+
+        GLuint CreateProgram()
+        {
+            return glCreateProgram();
+        }
+    } // namespace
 
     GraphicsShader::GraphicsShader(std::string vertexSrc, std::string fragmentSrc, std::string geometrySource)
     {
         GLuint vertShader = CompileShader(vertexSrc, GL_VERTEX_SHADER, "Vertex");
         GLuint geomShader = CompileShader(geometrySource, GL_GEOMETRY_SHADER, "Geometry");
         GLuint fragShader = CompileShader(fragmentSrc, GL_FRAGMENT_SHADER, "Fragment");
-        m_Shader          = CreateProgram();
+
+        if (vertShader == 0 || geomShader == 0 || fragShader == 0) {
+            if (vertShader != 0)
+                glDeleteShader(vertShader);
+            if (geomShader != 0)
+                glDeleteShader(geomShader);
+            if (fragShader != 0)
+                glDeleteShader(fragShader);
+            return;
+        }
+
+        m_Shader = CreateProgram();
         glAttachShader(m_Shader, vertShader);
         glAttachShader(m_Shader, geomShader);
         glAttachShader(m_Shader, fragShader);
         glLinkProgram(m_Shader);
         GLint isLinked = 0;
-        glGetProgramiv(m_Shader, GL_LINK_STATUS, (int *)&isLinked);
+        glGetProgramiv(m_Shader, GL_LINK_STATUS, &isLinked);
 
         if (isLinked == GL_FALSE) {
             GLint maxLength = 0;
@@ -26,9 +71,12 @@ namespace tf3d::base
             memset(errorLog, 0, maxLength);
             glGetProgramInfoLog(m_Shader, maxLength, &maxLength, errorLog);
             glDeleteProgram(m_Shader);
+            m_Shader = 0;
             glDeleteShader(vertShader);
+            glDeleteShader(geomShader);
             glDeleteShader(fragShader);
             TF3D_LOG_ERROR("Shader link failed: {}", errorLog);
+            free(errorLog);
             return;
         }
 
@@ -44,12 +92,21 @@ namespace tf3d::base
     {
         GLuint vertShader = CompileShader(vertexSrc, GL_VERTEX_SHADER, "Vertex");
         GLuint fragShader = CompileShader(fragmentSrc, GL_FRAGMENT_SHADER, "Fragment");
-        m_Shader          = CreateProgram();
+
+        if (vertShader == 0 || fragShader == 0) {
+            if (vertShader != 0)
+                glDeleteShader(vertShader);
+            if (fragShader != 0)
+                glDeleteShader(fragShader);
+            return;
+        }
+
+        m_Shader = CreateProgram();
         glAttachShader(m_Shader, vertShader);
         glAttachShader(m_Shader, fragShader);
         glLinkProgram(m_Shader);
         GLint isLinked = 0;
-        glGetProgramiv(m_Shader, GL_LINK_STATUS, (int *)&isLinked);
+        glGetProgramiv(m_Shader, GL_LINK_STATUS, &isLinked);
 
         if (isLinked == GL_FALSE) {
             GLint maxLength = 0;
@@ -58,9 +115,11 @@ namespace tf3d::base
             memset(errorLog, 0, maxLength);
             glGetProgramInfoLog(m_Shader, maxLength, &maxLength, errorLog);
             glDeleteProgram(m_Shader);
+            m_Shader = 0;
             glDeleteShader(vertShader);
             glDeleteShader(fragShader);
             TF3D_LOG_ERROR("Shader link failed: {}", errorLog);
+            free(errorLog);
             return;
         }
 
@@ -70,19 +129,18 @@ namespace tf3d::base
         glDeleteShader(fragShader);
     }
 
-    GraphicsShader::~GraphicsShader()
-    {
-        glDeleteProgram(m_Shader);
-    }
-
     ComputeShader::ComputeShader(std::string source)
     {
         GLuint shader = CompileShader(source, GL_COMPUTE_SHADER, "Compute Shader");
-        m_Shader      = CreateProgram();
+
+        if (shader == 0)
+            return;
+
+        m_Shader = CreateProgram();
         glAttachShader(m_Shader, shader);
         glLinkProgram(m_Shader);
         GLint isLinked = 0;
-        glGetProgramiv(m_Shader, GL_LINK_STATUS, (int *)&isLinked);
+        glGetProgramiv(m_Shader, GL_LINK_STATUS, &isLinked);
 
         if (isLinked == GL_FALSE) {
             GLint maxLength = 0;
@@ -91,8 +149,10 @@ namespace tf3d::base
             memset(errorLog, 0, maxLength);
             glGetProgramInfoLog(m_Shader, maxLength, &maxLength, errorLog);
             glDeleteProgram(m_Shader);
+            m_Shader = 0;
             glDeleteShader(shader);
             TF3D_LOG_ERROR("Compute shader link failed: {}", errorLog);
+            free(errorLog);
             return;
         }
 
@@ -105,11 +165,6 @@ namespace tf3d::base
         glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 1, &maxWorkGroupSize[1]);
         glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 2, &maxWorkGroupSize[2]);
         glGetIntegerv(GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &maxWorkGroupInvocations);
-    }
-
-    ComputeShader::~ComputeShader()
-    {
-        glDeleteProgram(m_Shader);
     }
 
 } // namespace tf3d::base
